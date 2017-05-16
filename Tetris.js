472b27993a312f9c500df6fad2e303fa8a404bea
bablet <frk.haha@hotmail.se>
2017-05-16 14:10:02 +0200

    modified:   Tetris.js Gjorde arrayen blocks redo för rotationer Ändrade variabeln c_block till ett objekt för att hålla nuvarande rotation, form och koordinater Ändringar med variablar i new_block() Ändringar med variablar i auto_move()

--- Tetris.js
+++ Tetris.js
@@ -1,65 +1,100 @@
 var board_width  = 9;
 var board_height = 15;
 var speed = 1000; 	// Hur snabbt blocket rör sig ner i milisekunder 
-var blocks = [ 		// former som man ger till nuvarande block (c_block)
-	[[1,1],[2,1],[3,1],[4,1]], // Linje
-	[[1,1],[2,1],[2,2],[3,1]], // T
-	[[1,1],[2,1],[1,2],[2,2]], // Kub
-	[[1,1],[2,1],[2,2],[3,2]], // Z
-	[[2,1],[3,1],[1,2],[2,2]], // Motsatt Z
-	[[1,1],[2,1],[3,1],[1,2]], // L
-	[[1,1],[2,1],[3,1],[3,2]]  // Motsatt L
-]
-
-var c_block; 		// Får sitt värde i new_block()
 var interval_id; 	// Får sitt värde i auto_move()
 
-function create_board(){ // Skapar bräda
-	for(let y = 1; y <= board_height; y++){				// iterera igenom y-led
-		document.write("<tr>");					// skapa början på en rad i en tabel
-		for(let x = 1; x <= board_width; x++){			// iterera igenom x-led
-			document.write("<td id=" + x + "," + y + " style=\"background-color:white\">"); // skriver ut en plats i tabelen, id är kordinaterna t.ex. id="3,6"
+var blocks = [	// Former som man ger till nuvarande block (c_block) och rotationer till alla block
+	[ 						//Första rotation [0]
+		[[1,1],[2,1],[3,1],[4,1]], // Linje
+		[[1,1],[2,1],[2,2],[3,1]], // T
+		[[1,1],[2,1],[1,2],[2,2]], // Kub
+		[[1,1],[2,1],[2,2],[3,2]], // Z
+		[[2,1],[3,1],[1,2],[2,2]], // Motsatt Z
+		[[1,1],[2,1],[3,1],[1,2]], // L
+		[[1,1],[2,1],[3,1],[3,2]]  // Motsatt L
+	],
+	[						// Andra rotation [1]
+		[[2,3],[2,2],[2,1],[2,0]], // Linje
+		[[2,0],[2,1],[1,1],[2,2]], // T
+		[[1,1],[2,1],[1,2],[2,2]], // Kub
+		[[1,1],[2,1],[1,2],[2,0]], // Z
+		[[1,0],[1,1],[2,1],[2,2]], // Motsatt Z
+		[[1,0],[2,0],[2,1],[2,2]], // L
+		[[2,0],[2,1],[2,2],[1,2]]  // Motsatt L
+	],
+	[						// Tredje rotation [2]
+		[[1,1],[2,1],[3,1],[4,1]], // Linje
+		[[1,1],[2,1],[2,0],[3,1]], // T
+		[[1,1],[2,1],[1,2],[2,2]], // Kub
+		[[1,1],[2,1],[2,2],[3,2]], // Z
+		[[2,1],[3,1],[1,2],[2,2]], // Motsatt Z
+		[[3,0],[3,1],[2,1],[1,1]], // L
+		[[1,0],[1,1],[2,1],[3,1]]  // Motsatt L
+	],
+	[						// Fjärde rotation [3]
+		[[2,3],[2,2],[2,1],[2,0]], // Linje
+		[[2,0],[2,1],[2,2],[3,1]], // T
+		[[1,1],[2,1],[1,2],[2,2]], // Kub
+		[[1,1],[2,1],[1,2],[2,0]], // Z
+		[[1,0],[1,1],[2,1],[2,2]], // Motsatt Z
+		[[2,0],[2,1],[2,2],[3,2]], // L
+		[[2,0],[3,0],[2,1],[2,2]]  // Motsatt L
+	]
+]
+
+var c_block = new Object();		// Får sinna nycklar (rotation, coords, id) och värden i new_block()
+
+function create_board(){ 						// Skapar bräda
+	for(let y = 1; y <= board_height; y++){						// Iterera igenom y-led
+		document.write("<tr>");							// Skapa början på en rad i en tabel
+		for(let x = 1; x <= board_width; x++){					// Iterera igenom x-led
+			document.write( "<td id=" + x + "," + y + 
+					" style=\"background-color:white\">"); 		// Skriver ut en plats i tabelen, id är kordinaterna t.ex. id="3,6"
 		}
-		document.write("</tr>");				// stäng raden i tabelen.
+		document.write("</tr>");						// Stäng raden i tabelen.
 	}
 }
 
-function new_block(){ //Skapar ett nytt block med en form från blocks[]
-	c_block = blocks[Math.floor(Math.random() * blocks.length)]; 	// Ge ny form av block till nuvarande block
-	draw_block_on_board(); 						// skriv sedan ut blocket
+function new_block(){ 							//Skapar ett nytt block med en form från blocks[]
+	let r_number = Math.floor(Math.random() * blocks[0].length);
+	c_block.rotation = 0; 								// Ge först första rotation
+	c_block.coords = blocks[c_block.rotation][r_number]; 				// Ge ny form av block till nuvarande block med första rotation
+	c_block.id = r_number;								// Ge id för formen så att vi kan hitta rätt rotationer i blocks[]
+	draw_block_on_board(); 								// Skriv sedan ut blocket
 };
 
-function draw_block_on_board(){ // skriver ut nuvarande blocket
-	c_block = blocks[Math.floor(Math.random() * blocks.length)]; 	// Ge nuvarande blocket en form
-	for(let i in c_block){						// itererar igenom alla arrayer som inehåller kordinater i nuvarande block
-		change_color(c_block[i][0], c_block[i][1], "blue"); 	// ändrar färg på spelplan på de kordinaterna som nuvarande blocket har
+function draw_block_on_board(){ 							// Skriver ut nuvarande blocket
+	for(let i in c_block.coords){							// Itererar igenom alla arrayer som inehåller kordinater i nuvarande block
+		change_color(c_block.coords[i][0], c_block.coords[i][1], "blue"); 	// Ändrar färg på spelplan på de kordinaterna som nuvarande blocket har
 	}
 }
 
-function change_color(x,y,color){ // ändrar till angiven färg på angivna kordinater i spelplanen
-	document.getElementById(x+","+y).style.background = color;
+function change_color(x,y,color){ 					// Ändrar till angiven färg på angivna kordinater i spelplanen
+	if(x > 0 && y > 0){
+		document.getElementById(x+","+y).style.background = color;
+	}
 }
 
-function check_under(){ // Kollar om nuvarande block har någonting under sig
+function auto_move(){ 							// Rör blocket neråt i ett interval
+	interval_id = setInterval(function(){ 						// Skapar ett interval som en function körs i. Intervalet returnar en id som man sedan kan avända för att stoppa intervalet. 
+		let t_coords = []; 							// För temporära kordinater
+		for(let i in c_block.coords){						// Ge alla kordinater från nuvarande block men y+1 till temporära kordinater 
+			t_coords.push([c_block.coords[i][0], c_block.coords[i][1] + 1]);
+		}
+		for(let i in c_block.cords){						// Ta bort nuvarande block.
+			change_color(c_block.coords[i][0], c_block.coords[i][1], "white");
+		}
+		for(let i in t_coords){							// Skriv ut nytt block med tämporära kordinater
+			change_color(t_coords[i][0], t_coords[i][1], "blue");
+		}
+		c_block.coords = t_coords; 						// Ge kordinater från tämporära kordinater till nuvarande block
+	}, speed); 									// speed = Antal milisekunder innan functionen körs igen
+}
+
+function check_under(){ 						// Kollar om nuvarande block har någonting under sig
 
 }
 
-function auto_move(){ // Rör blocket neråt i ett interval
-	interval_id = setInterval(function(){ 				// skapar ett interval som en function körs i. Intervalet returnar en id som man sedan kan avända för att stoppa intervalet. 
-		let t_block = []; 					// nytt temporärt block
-		for(let i in c_block){					// Ge nytt block alla kordinater från nuvarande block men y+1 
-			t_block.push([c_block[i][0], c_block[i][1] + 1]);
-		}
-		for(let i in c_block){					// Ta bort nuvarande block.
-			change_color(c_block[i][0], c_block[i][1], "white");
-		}
-		for(let i in t_block){					// Skriv ut nytt block.
-			change_color(t_block[i][0], t_block[i][1], "blue");
-		}
-		c_block = t_block; 					// Ge kordinater från nytt block till nuvarande block
-	}, speed); 							// speed = antal milisekunder innan functionen körs igen
-}
-
-function rotate(){ // Roterar nuvarande block
+function rotate(){ 							// Roterar nuvarande block
 
 }
