0debb4cd4fbee47c66fdd8433dc427f310d52fec
bablet <frk.haha@hotmail.se>
2017-05-24 20:28:10 +0200

    modified:   Tetris.js Fixade kommentarerna

--- Tetris.js
+++ Tetris.js
@@ -1,9 +1,16 @@
+//Brädan
 var board_width  = 9;
 var board_height = 15;
 var board_color = "grey";
-var speed = 1000; 	// Hur snabbt blocket rör sig ner i milisekunder 
-var interval_id; 	// Får sitt värde i auto_move()
-var blocks = [	// Former som man ger till nuvarande block (c_block) och rotationer till alla block
+
+// Hur snabbt blocket rör sig ner i milisekunder 
+var speed = 1000;
+
+// Interval som rör blocket neråt. Får sitt värde i auto_move().
+var interval_auto_move;
+
+// Former som man ger till nuvarande block (c_block) och rotationer till alla block
+var blocks = [	
 	[ 						//Första rotation [0]
 		[[1,1],[2,1],[3,1],[4,1]], // Linje
 		[[1,1],[2,1],[2,2],[3,1]], // T
@@ -41,69 +48,113 @@ var blocks = [	// Former som man ger till nuvarande block (c_block) och rotation
 		[[2,0],[3,0],[2,1],[2,2]]  // Motsatt L
 	]
 ];
-var blocks_color = ["blue", "red", "green", "purple", "yellow", "orange", "white"];	// Färger för blocks
 
-var c_block = new Object();		// Får sinna nycklar (rotation, coords, id) och värden i new_block()
+// Färger för blocks.
+var blocks_color = ["blue", "red", "green", "purple", "yellow", "orange", "white"];	// Färger för blocks.
 
-function create_board(){ 						// Skapar bräda
-	for(let y = 1; y <= board_height; y++){						// Iterera igenom y-led
-		document.write("<tr>");							// Skapa början på en rad i en tabel
-		for(let x = 1; x <= board_width; x++){					// Iterera igenom x-led
-			document.write( "<td id=" + x + "," + y + " style=\"background-color:" + board_color + "\">"); // Skriver ut en plats i tabelen, id är kordinaterna t.ex. id="3,6"
+// Nuvarande block, blocket som blir manipulerat. Får sinna nycklar (rotation, coords, id) och värden i new_block().
+var c_block = new Object();
+
+// Skapar bräda.
+function create_board(){ 
+	// Iterera igenom y-led.
+	for(let y = 1; y <= board_height; y++){
+		// Skapa början på en rad i en tabel.
+		document.write("<tr>");
+		// Iterera igenom x-led.							
+		for(let x = 1; x <= board_width; x++){
+			// Skriv ut en plats i tabelen, id är kordinaterna t.ex. id="3,6".
+			document.write( "<td id=" + x + "," + y + " style=\"background-color:" + board_color + "\">");
 		}
-		document.write("</tr>");						// Stäng raden i tabelen.
+		// Stäng raden i tabelen.
+		document.write("</tr>");
 	}
 }
 
-function new_block(){ 							//Skapar ett nytt block med en form från blocks[]
+//Skapar ett nytt block / Ger nya värden till nuvarande block.
+function new_block(){
+	// Skriver ut nuvarande blocket.
+	function draw_block_on_board(){
+		// Itererar igenom alla arrayer som inehåller kordinater i nuvarande block.
+		for(let i in c_block.coords){
+			// Ändra färg på spelplan på de kordinaterna som nuvarande blocket har.
+			change_color(c_block.coords[i][0], c_block.coords[i][1], c_block.color);
+		}
+	}
+
 	let r_number = Math.floor(Math.random() * blocks[0].length);
-	c_block.rotation = 0; 								// Ge blocket första rotation
-	c_block.coords = blocks[c_block.rotation][r_number]; 				// Ge ny form av block till nuvarande block med första rotation
-	c_block.id = r_number;								// Ge id för formen så att man kan hitta rotationer för block i blocks[]
-	c_block.color = blocks_color[r_number];						// Ge färg till block
-	c_block.indexes_lower_coords = find_lower_coords();					// Ge kordinaterna för de understa blocken, används med check_under();
-	draw_block_on_board(); 								// Skriv sedan ut blocket
+	// Ge blocket första rotation.
+	c_block.rotation = 0;
+	// Ge ny form av block till nuvarande block med första rotation.								
+	c_block.coords = blocks[c_block.rotation][c_block.rotation];
+	// Ge id för formen så att man kan hitta rotationer för block i blocks[].
+	c_block.id = r_number;
+	// Ge färg till block.
+	c_block.color = blocks_color[r_number];
+	// Hitta de indexen med de understa blocken.
+	c_block.indexes_lower_coords = find_lower_coords();
+
+	// Hitta de indexen med de högra blocken.
+	//c_block.indexes_right_coords = find_right_coords();
+
+	// Hitta de indexen med de vänstra blocken.
+	//c_block.indexes_left_coords = find_left_coords();
+	
+	// Skriv sedan ut blocket.
+	draw_block_on_board();
 };
 
-function draw_block_on_board(){ 							// Skriver ut nuvarande blocket
-	for(let i in c_block.coords){							// Itererar igenom alla arrayer som inehåller kordinater i nuvarande block
-		change_color(c_block.coords[i][0], c_block.coords[i][1], c_block.color); 	// Ändrar färg på spelplan på de kordinaterna som nuvarande blocket har
-	}
-}
-
-function change_color(x,y,color){ 					// Ändrar till angiven färg på angivna kordinater i spelplanen
+// Ändrar till angiven färg på angivna kordinater i spelplanen
+function change_color(x,y,color){
 	if(x > 0 && y > 0){
 		document.getElementById(x+","+y).style.backgroundColor = color;
 	}
 }
 
-function auto_move(){ 							// Rör blocket neråt i ett interval
-	interval_id = setInterval(function(){ 						// Skapar ett interval som en function körs i. Intervalet returnar en id som man sedan kan avända för att stoppa intervalet. 
-		let t_coords = []; 							// För temporära kordinater
-		if(check_under() == false){							// Kolla under blocket först
-			for(let i in c_block.coords){						// Ge alla kordinater från nuvarande block men y+1 till temporära kordinater 
+// Rör blocket neråt i ett interval
+function auto_move(){
+	// Skapar ett interval. Intervalet returnar en id som man sedan kan avända för att stoppa intervalet. 
+	interval_auto_move = setInterval(function(){
+		// För temporära kordinater
+		let t_coords = [];
+
+		// Kolla under blocket först
+		if(check_under() == false){
+			// Ge alla kordinater från nuvarande block men y+1 till temporära kordinater
+			for(let i in c_block.coords){
 				t_coords.push([c_block.coords[i][0], c_block.coords[i][1] + 1]);
 			}
-			for(let i in c_block.coords){						// Ta bort nuvarande block.
+			// Ta bort nuvarande block.
+			for(let i in c_block.coords){
 				change_color(c_block.coords[i][0], c_block.coords[i][1], board_color);
 			}
-			for(let i in t_coords){							// Skriv ut nytt block med tämporära kordinater
+			// Skriv ut nytt block med tämporära kordinater
+			for(let i in t_coords){
 				change_color(t_coords[i][0], t_coords[i][1], c_block.color);
 			}
-			c_block.coords = t_coords; 						// Ge kordinater från tämporära kordinater till nuvarande block
+			// Ge kordinater från tämporära kordinater till nuvarande block
+			c_block.coords = t_coords;
 		} else {
+			//Om nuvarande block har något under sig. Gör ett nytt block.
 			new_block();
 		}
-	}, speed); 									// speed = Antal milisekunder innan functionen körs igen
+	// speed = Antal milisekunder innan functionen körs igen.
+	}, speed);
 }
 
+// Hittar de lägsta kordinaterna för blocket, används varje gång man det skapas ett nytt block eller när man roterar blocket.
+function find_lower_coords(){
+	// Håller indexen för c_block.coords[] om det finns fler av sanna x kordinat
+	let indexes_duplicate_x_coords = [];
 
-function find_lower_coords(){						// Hittar de lägsta kordinaterna för blocket, används varje gång man det skapas ett nytt block eller när man roterar blocket.
-	let indexes_duplicate_x_coords = [];						// Håller indexen för c_block.coords[] om det finns fler av sanna x kordinat 
-	let x_coords_checked = [];							// Håller x kordinater som redan har används
-	let indexes_lower_coords = [];							// Array som håller de lägsta kordinaterna som hittats
+	// Håller x kordinater som redan har används
+	let x_coords_checked = [];
 
-	function already_checked(coord){						// Ser om man redan lettat efter given x kordinat
+	// Array som håller de lägsta kordinaterna som hittats
+	let indexes_lower_coords = [];
+
+	// Ser om man redan lettat efter given x kordinat
+	function already_checked(coord){
 		for(let i in x_coords_checked){
 			if(x_coords_checked[i] == coord){
 				return true;
@@ -112,7 +163,8 @@ function find_lower_coords(){						// Hittar de lägsta kordinaterna för blocke
 		return false;
 	}
 
-	function check_y_coords_with_x(indexes_of_x_coords){		// Fungerar...
+	// Fungerar...
+	function check_y_coords_with_x(indexes_of_x_coords){
 		let index_of_lowest_y_coord = indexes_of_x_coords[0];
 
 		for(let i in indexes_of_x_coords){
@@ -123,7 +175,8 @@ function find_lower_coords(){						// Hittar de lägsta kordinaterna för blocke
 		indexes_lower_coords.push(index_of_lowest_y_coord);
 	}
 
-	function check_x_coords(){					// Fungerar...
+	// Fungerar...
+	function check_x_coords(){
 		for(let i in c_block.coords){
 			if(already_checked(c_block.coords[i][0]) == false){
 				x_coords_checked.push(c_block.coords[i][0]);
@@ -153,19 +206,21 @@ function find_lower_coords(){						// Hittar de lägsta kordinaterna för blocke
 	return indexes_lower_coords;
 }
 
-function find_left_coords(){						// Hitta kordinater längst till vänster
+// Hitta kordinater längst till vänster
+function find_left_coords(){
 
 }
 
-function find_right_coords(){						// Hitta kordianter längst till höger
+// Hitta kordianter längst till höger
+function find_right_coords(){
 
 }
 
-function check_under(){ 						// Kollar om nuvarande block har någonting under sig
+// Kollar om nuvarande block har någonting under sig
+function check_under(){
 	let x_coord;
 	let y_coord;
 	for(let i = 0; i < c_block.indexes_lower_coords.length; i++){
-		console.log(c_block.indexes_lower_coords[i]);
 		x_coord = c_block.coords[c_block.indexes_lower_coords[i]][0];
 		y_coord = c_block.coords[c_block.indexes_lower_coords[i]][1] + 1;
 		if(y_coord > board_height || (document.getElementById(x_coord + "," + y_coord).style.backgroundColor != board_color)){
@@ -176,20 +231,24 @@ function check_under(){ 						// Kollar om nuvarande block har någonting under 
 	}
 }
 
-function move_left(){ 							// Rör block åt vänster
+// Rör block åt vänster
+function move_left(){
+	// Kollar om nuvarande block har något till vänster om sig, om den har det, return true.
 	function check_left(){
 
 	}
 
 }
 
-function move_right(){ 							// Rör block åt höger
+// Rör block åt höger
+function move_right(){
+	// Kollar om nuvarande block har något till höger om sig, om den har det, return true.
 	function check_right(){
 
 	}
 
 }
-
-function rotate(){ 							// Roterar nuvarande block
+// Roterar nuvarande block
+function rotate(){
 		
 }
